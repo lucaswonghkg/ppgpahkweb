48333e90b37d1123da017ab620a933c0c3010433
Lucas Wong <lucaswonghkg@gmail.com>
2021-08-31 01:40:14 +0800

    imgur picture 2

--- manual1.js
+++ manual1.js
@@ -1,34 +1,57 @@
 
-const api_url = 'https://api.imgur.com/3/account/me/images?client_id=5f108e8a434e8b5';
+var settings = {
+    "url": "https://api.imgur.com/3/account/me/images",
+    "method": "GET",
+    "timeout": 0,
+    "headers": {
+      "Authorization": "Bearer 7b3be15fd96986da6f5e2eeb591b0f88dd22f517"
+    },
+  };
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  var objlength = Object.keys(response.data).length
+// console.log(objlength);
+
+    for(let i = 0 ; i < objlength ; i++){
+        const { id, title, description} = response.data[i];
+        picurl = 'https:i.imgur.com/' +id+ '.jpg';
+        console.log(picurl)
 
 
-let picurl;
-let manarr =[];
-let displayTemp;
-
-async function getPic(){
-const response = await fetch(api_url);
-const data = await response.json();
-const totalnumber = data.photos.total;
+        if(description == "report_ml"){
+            document.getElementById('manual1').innerHTML += "<img src=" +picurl + ">" ;
+         }
+    
+   
+    }
 
 
-for(let i = 0 ; i< totalnumber ; i++){
-const { id, title, description} = data.photos.photo[i];
-picurl = `https:i.imgur.com/`+id+'.jpg';
-manarr.push(picurl);
-console.log(data);
-console.log(manarr[i]);
-// document.getElementById('manual1').innerHTML += "<img src=" +manarr[i] + ">" ;
+
+
+
+
+    
 }
 
 
 
 
 
-}
+
+
+
+// for(let i = 0 ; i< totalnumber ; i++){
+// const { id, title, description} = data[i];
+// picurl = `https:i.imgur.com/`+id+'.jpg';
+// manarr.push(picurl);
+// console.log(data);
+// console.log(manarr[i]);
+// // document.getElementById('manual1').innerHTML += "<img src=" +manarr[i] + ">" ;
+// }
 
 
 
 
 
-getPic();
+  );
